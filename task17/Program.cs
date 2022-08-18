d67c35cc0694030a45f27a0f51a32c4fdd44f86c
Artemiy <a89155357660@gmail.com>
2022-08-18 11:04:30 +0300

    homework

---
+++ task17/Program.cs
@@ -0,0 +1,10 @@
+﻿int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0)
+Console.WriteLine("I");
+if (x < 0 && y > 0)
+Console.WriteLine("II");
+if (x < 0 && y < 0)
+Console.WriteLine("III");
+if (x > 0 && y < 0)
+Console.WriteLine("IV");
